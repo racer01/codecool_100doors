c25e165399911b5bfe51ac7f3a9edb6b9c8c6183
László Székely-Tóth <notisracer@gmail.com>
2016-11-02 11:51:10 +0100

    v1.0.0

---
+++ 100doors.py
@@ -0,0 +1,28 @@
+# 100doors
+
+doors = []
+
+
+# 2: 2, 4, 6, 8
+# 3: 3, 6, 9
+def OpenDoors(n):
+    counter = n
+    while counter < len(doors):
+        doors[counter] = not doors[counter]
+        counter += n
+
+
+def main():
+    for i in range(101):  # init; 101 because 0->100
+        doors.append(False)
+
+    for i in range(1, len(doors)+1):
+        OpenDoors(i)
+    opened = []
+    for i in range(1, len(doors)):
+        if doors[i]:
+            # print(i)
+            opened.append(str(i))
+    print("The following doors are open:")
+    print(", ".join(opened))
+main()
